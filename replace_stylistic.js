f1d9d50c61ec34fbbcc948798ce5b29cb87d4fa4
spaghettian94 <timer-b@protonmail.com>
2019-09-26 04:29:35 +0900

    fix: use promisify exec

--- replace_stylistic.js
+++ replace_stylistic.js
@@ -1,6 +1,8 @@
-var fs = require('fs');
-var exec = require('child_process').exec;
-var { Parser, Builder } = require('xml2js');
+const promisify = require('util').promisify;
+const fs = require('fs');
+const { Parser, Builder } = require('xml2js');
+
+const exec = promisify(require('child_process').exec);
 
 if (process.argv.length < 3) {
   console.log('usage: $ node replace_stylistic.js {fontfile.ttf / fontfile.otf}');
@@ -13,37 +15,41 @@ const originFileName = originFilePath.replace(/^.*\/(.+)\..+$/, '$1');
 const ttxFilePath = `./temp/${originFileName}`;
 const outputPath = originFilePath.replace(/^.*\/(.+)$/, './outputs/$1');
 
-// TTXファイル生成
-const commandDecompile = `ttx -f -t CFF -o ${ttxFilePath}.ttx ${originFilePath}`;
-exec(commandDecompile, async (err, stdout, stderr) => {
-  console.log(commandDecompile);
-  console.log(stdout, stderr);
+main();
 
+async function main() {
+  // TTXファイル生成
+  const commandDecompile = `ttx -f -t CFF -o ${ttxFilePath}.ttx ${originFilePath}`;
+  await exec(commandDecompile).then(({stdout, stderr}) => {
+    console.log(commandDecompile);
+    console.log(stdout, stderr);
+  });
+  
   // TTXファイル読込
   let ttxContent = fs.readFileSync(`${ttxFilePath}.ttx`) + '';
   ttxContent = ttxContent.replace(/\r/g, '').replace(/\uFEFF/g, '').replace(/\n*$/, '');
-
+  
   // XML書式からJSONデータに加工
   const parser = new Parser();
   const ttxObject = await parser.parseStringPromise(ttxContent);
-
+  
   // 文字の置き換え
   enableStylisticSet01(ttxObject);
   enableStylisticSet02(ttxObject);
   enableStylisticSet19(ttxObject);
-
+  
   // JSONデータをXML書式に書き戻し～TTXファイルに保存
   const builder = new Builder();
   ttxContent = builder.buildObject(ttxObject);
   fs.writeFileSync(`${ttxFilePath}_new.ttx`, ttxContent);
-
+  
   // 変更したTTXファイルと元フォントを合成
   const commandCompile = `ttx -o ${outputPath} -m ${originFilePath} ${ttxFilePath}_new.ttx`;
-  exec(commandCompile, function(err, stdout, stderr){
+  await exec(commandCompile).then(({stdout, stderr}) => {
     console.log(commandCompile);
     console.log(stdout+stderr);
   });
-});
+}
 
 function getCharStrings(ttxObject) {
   try {
